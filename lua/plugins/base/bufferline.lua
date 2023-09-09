66b84dfcd13f0e92324b9a597363a7bece6e8da4
mbecker20 <becker.maxh@gmail.com>
2023-09-09 01:01:37 -0400

    working pretty decently

--- lua/plugins/base/bufferline.lua
+++ lua/plugins/base/bufferline.lua
@@ -6,6 +6,10 @@ return {
 		options = {
 			mode = "tabs",
 			diagnostics = "nvim_lsp",
+			diagnostics_indicator = function(count, level, diagnostics_dict, context)
+				local icon = level:match("error") and " " or " "
+				return " " .. icon .. count
+			end,
 		},
 	},
 }
--- lua/plugins/base/telescope.lua
+++ lua/plugins/base/telescope.lua
@@ -1,40 +1,41 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-    telescope.setup({
-      defaults = {
-        path_display = { "truncate " },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
-        },
-      },
-    })
+		telescope.setup({
+			defaults = {
+				path_display = { "truncate " },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+		})
 
-    telescope.load_extension("fzf")
-    
-    local wk = require("which-key")
+		telescope.load_extension("fzf")
 
-    wk.register({
-      f = {
-        name = "find", -- optional group name
-        f = { "<cmd>Telescope find_files<cr>", "Find file" },
-        r = { "<cmd>Telescope oldfiles<cr>", "Open recent file" },
-        s = { "<cmd>Telescope live_grep<cr>", "Find string" },
-        c = { "<cmd>Telescope grep_string<cr>", "Find string under cursor" }
-      },
-    }, { prefix = "<leader>" })
-  end,
+		local wk = require("which-key")
+
+		wk.register({
+			f = {
+				name = "find", -- optional group name
+				f = { "<cmd>Telescope find_files<cr>", "Find file" },
+				r = { "<cmd>Telescope oldfiles<cr>", "Open recent file" },
+				s = { "<cmd>Telescope live_grep<cr>", "Find string" },
+				c = { "<cmd>Telescope grep_string<cr>", "Find string under cursor" },
+				b = { "<cmd>Telescope buffer<cr>", "Show buffers" },
+			},
+		}, { prefix = "<leader>" })
+	end,
 }
