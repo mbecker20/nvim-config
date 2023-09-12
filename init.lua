83178d4c382d8ca518ee18a2b42ac90b3ecf288b
mbecker20 <becker.maxh@gmail.com>
2023-09-11 23:42:29 -0400

    actually add treesitter

--- init.lua
+++ init.lua
@@ -3,30 +3,31 @@ require("core.keymaps")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { import = "plugins.base" },
-  { import = "plugins.cmp" },
-  { import = "plugins.lsp" },
+	{ import = "plugins.base" },
+	{ import = "plugins.cmp" },
+	{ import = "plugins.lsp" },
+	{ import = "plugins.treesitter" },
 }, {
-  install = {
-    colorscheme = { "nightfly" },
-  },
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
+	install = {
+		colorscheme = { "nightfly" },
+	},
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
