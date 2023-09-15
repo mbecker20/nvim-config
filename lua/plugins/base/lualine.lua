3485c5f535316250adba9ed2c6639e34bdd22246
mbecker20 <becker.maxh@gmail.com>
2023-09-15 01:50:38 -0400

    reorder lualine

--- lua/plugins/base/lualine.lua
+++ lua/plugins/base/lualine.lua
@@ -15,15 +15,14 @@ return {
 			},
 			sections = {
 				lualine_x = {
+					{ "diagnostics", sources = { "nvim_lsp" } },
 					{
 						lazy_status.updates,
 						cond = lazy_status.has_updates,
 						-- color = { fg = "#ff9e64" },
 					},
 					{ "encoding" },
-					-- { "fileformat" },
 					{ "filetype" },
-					{ "diagnostics", sources = { "nvim_lsp" } },
 				},
 			},
 		})
