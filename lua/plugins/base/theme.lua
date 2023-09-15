ecd58f7ab41a5b218c2073b1d5ce2c352a6a2eef
mbecker20 <becker.maxh@gmail.com>
2023-09-14 23:19:37 -0400

    remove line numbers from file tree

--- lua/plugins/base/theme.lua
+++ lua/plugins/base/theme.lua
@@ -1,10 +1,9 @@
 return {
-  {
-    "bluz71/vim-nightfly-guicolors",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme nightfly]])
-    end,
-  },
+	"bluz71/vim-nightfly-guicolors",
+	-- "rebelot/kanagawa.nvim",
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		vim.cmd([[colorscheme nightfly]])
+		-- vim.cmd([[colorscheme kanagawa]])
+	end,
 }
